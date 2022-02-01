61404c7eb77942b1c0c17765bf3104cb14e2750a
paul.t <paul.t@gembaadvantage.com>
2022-02-01 20:27:10 +0000

    update date on license

--- pkg/translate/dissect.go
+++ pkg/translate/dissect.go
@@ -20,46 +20,4 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-package main
-
-import (
-	"fmt"
-	"io"
-
-	"github.com/gembaadvantage/codecommit-sign/internal/version"
-	"github.com/spf13/cobra"
-)
-
-type versionOptions struct {
-	short bool
-}
-
-func newVersionCmd(out io.Writer) *cobra.Command {
-	opts := versionOptions{}
-
-	cmd := &cobra.Command{
-		Use:   "version",
-		Short: "Prints the build time version information",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return opts.run(out)
-		},
-	}
-
-	f := cmd.Flags()
-	f.BoolVar(&opts.short, "short", false, "only print the semantic version number")
-
-	return cmd
-}
-
-func (o versionOptions) run(out io.Writer) error {
-	fmt.Fprintln(out, formatVersion(o.short))
-	return nil
-}
-
-func formatVersion(short bool) string {
-	if short {
-		return version.Short()
-	}
-
-	return fmt.Sprintf("%#v", version.Long())
-}
+package translate
